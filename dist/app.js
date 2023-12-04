1637a1160c800290684f2f5ea294898741fb28d8
yadavsk098 <yadavsk098@gmail.com>
2023-12-04 22:55:34 +0530

    added some files

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+// app.js
+// Use JavaScript to handle user interactions and chat logic
+
+function sendMessage() {
+    // Get user input
+    const userInput = document.getElementById('user-input').value;
+
+    // TODO: Send the user input to the server, process it, and update the chat container
+    // You'll need to implement the server-side logic for handling the message
+}
